3e840e64f9d359e3d6e673e6a76295c67e53dfba
Digi <marton@szabomarci.hu>
2023-03-17 17:27:53 +0100

    Skálázható lett az oldal a lángosokat tekintve.

--- osszegzes.js
+++ osszegzes.js
@@ -1,7 +1,51 @@
 var rendelesek = JSON.parse(localStorage.getItem('rendelesekJSON'));
 
-let total_amount = rendelesek.nutellas[0] * rendelesek.nutellas[1] + rendelesek.bitang[1] * rendelesek.bitang[0] + rendelesek.sajtostejfolos[1] * rendelesek.sajtostejfolos[0] + rendelesek.tejfolos[1] * rendelesek.tejfolos[0] + rendelesek.sajtos[1] * rendelesek.sajtos[0] + rendelesek.sima[1] * rendelesek.sima[0];
+let total_amount = 0;
 
+for (let k = 1; k < 7; k++){
+    let x = Number(rendelesek[`langos_${k}`][0]);
+    let y = Number(rendelesek[`langos_${k}`][1]);
+    total_amount += (x*y);
+}
+
+
+
+function langos_n(){
+    for (let j = 1; j < 7; j++){
+        for (let i = 1; i < 4; i++){
+            if (i == 1){
+                htmltext = `${rendelesek[`langos_${j}`][i]} db`;
+            } 
+            else 
+            { if (rendelesek[`langos_${j}`][i] == false){
+                htmltext = `<i class="fa-solid fa-circle-xmark"></i>`;
+            } else {
+                htmltext = `<i class="fa-sharp fa-solid fa-circle-check"></i>`;
+            }}
+            document.getElementById(`langos_${j}_${i}`).innerHTML=htmltext;
+            console.log(rendelesek[`langos_${j}`][i]);
+        }
+    }
+}
+
+
+
+function kiir(){
+    langos_n();
+    console.log(total_amount);
+    document.getElementById("total_amount").innerHTML= `${total_amount} Ft`;
+}
+
+function langos_arrive(){
+    let x = 0;
+    for (let i = 1; i <= 6; i++){
+        x = rendelesek[`langos_${i}`][1];
+    }
+    alert(`A lángosa megérkezik önhöz ${x*3 + 8} perc múlva`);
+}
+
+
+/*
 function sima(){
     for (let i = 1; i < 4; i+=1){
         if (i == 1){
@@ -16,95 +60,4 @@ function sima(){
         document.getElementById(`sima_${i}`).innerHTML=htmltext;
     }
 }
-
-function sajtos(){
-    for (let i = 1; i < 4; i+=1){
-        if (i == 1){
-            htmltext = `${rendelesek.sajtos[i]} db`;
-        } else {
-            if (rendelesek.sajtos[i] == false){
-                htmltext = `<i class="fa-solid fa-circle-xmark"></i>`;
-            } else {
-                htmltext = `<i class="fa-sharp fa-solid fa-circle-check"></i>`;
-            }
-        }
-        document.getElementById(`sajtos_${i}`).innerHTML=htmltext;
-    }
-}
-
-function tejfolos(){
-    for (let i = 1; i < 4; i+=1){
-        if (i == 1){
-            htmltext = `${rendelesek.tejfolos[i]} db`;
-        } else {
-            if (rendelesek.tejfolos[i] == false){
-                htmltext = `<i class="fa-solid fa-circle-xmark"></i>`;
-            } else {
-                htmltext = `<i class="fa-sharp fa-solid fa-circle-check"></i>`;
-            }
-        }
-        document.getElementById(`tejfolos_${i}`).innerHTML=htmltext;
-    }
-}
-
-function sajtostejfolos(){
-    for (let i = 1; i < 4; i+=1){
-        if (i == 1){
-            htmltext = `${rendelesek.sajtostejfolos[i]} db`;
-        } else {
-            if (rendelesek.sajtostejfolos[i] == false){
-                htmltext = `<i class="fa-solid fa-circle-xmark"></i>`;
-            } else {
-                htmltext = `<i class="fa-sharp fa-solid fa-circle-check"></i>`;
-            }
-        }
-        document.getElementById(`sajtostejfolos_${i}`).innerHTML=htmltext;
-    }
-}
-
-function bitang(){
-    for (let i = 1; i < 4; i+=1){
-        if (i == 1){
-            htmltext = `${rendelesek.bitang[i]} db`;
-        } else {
-            if (rendelesek.bitang[i] == false){
-                htmltext = `<i class="fa-solid fa-circle-xmark"></i>`;
-            } else {
-                htmltext = `<i class="fa-sharp fa-solid fa-circle-check"></i>`;
-            }
-        }
-        document.getElementById(`bitang_${i}`).innerHTML=htmltext;
-    }
-}
-
-function nutellas(){
-    for (let i = 1; i < 4; i+=1){
-        if (i == 1){
-            htmltext = `${rendelesek.nutellas[i]} db`;
-        } else {
-            if (rendelesek.nutellas[i] == false){
-                htmltext = `<i class="fa-solid fa-circle-xmark"></i>`;
-            } else {
-                htmltext = `<i class="fa-sharp fa-solid fa-circle-check"></i>`;
-            }
-        }
-        document.getElementById(`nutellas_${i}`).innerHTML=htmltext;
-    }
-}
-
-
-
-function kiir(){
-    sima();
-    sajtos();
-    tejfolos();
-    sajtostejfolos();
-    bitang();
-    nutellas();
-    document.getElementById("total_amount").innerHTML= `${total_amount} Ft`;
-}
-
-function langos_arrive(){
-    let x = Number(rendelesek.sima[1]) + Number(rendelesek.tejfolos[1]) + Number(rendelesek.sajtos[1]) + Number(rendelesek.sajtostejfolos[1]) + Number(rendelesek.bitang[1]) + Number(rendelesek.nutellas[1]); 
-    alert(`A lángosa megérkezik önhöz ${x*3 + 8} perc múlva`)
-}
+*/
--- rendeles.js
+++ rendeles.js
@@ -1,80 +1,35 @@
-/*------------------ár--mennyiség-só-fokhagyma-------*/
-
+/*------------------ár--mennyiség-só-fokhagyma-megnevezés------*/
 var rendelesek = {
-    "sima" : [800, 0, false, false],
-    "sajtos" : [1150, 0, false, false],
-    "tejfolos" : [1200, 0, false, false],
-    "sajtostejfolos" : [1350, 0, false, false],
-    "bitang" : [2000, 0, false, false],
-    "nutellas" : [1700, 0, false, false],
+    "langos_1" : [800, 0, false, false, "Sima"],
+    "langos_2" : [1150, 0, false, false, "Sajtos"],
+    "langos_3" : [1200, 0, false, false, "Tejfölös"],
+    "langos_4" : [1350, 0, false, false, "Sajtos-tejfölös"],
+    "langos_5" : [2000, 0, false, false, "Bitang"],
+    "langos_6" : [1700, 0, false, false, "Nutellás"],
 };
+
+let butt_id = 0;
+
+function load(){
+    console.log(rendelesek[`langos_${id}`][1]);
+    console.log(rendelesek[`langos_${id}`][2]);
+}
+
 
 function open_orders(){
     localStorage.setItem('rendelesekJSON', JSON.stringify(rendelesek));
-    window.location.href="osszegzes.html"
+    window.location.href="osszegzes.html";
 }
 
-function add_to_cart_sima(){
-    rendelesek.sima[1] = document.getElementById("sima_db").value;
-    rendelesek.sima[2] = document.getElementById("sima_so").checked;
-    rendelesek.sima[3] = document.getElementById("sima_fok").checked;
-    if (rendelesek.sima[1] == 0){
-        rendelesek.sima[2] = false;
-        rendelesek.sima[3] = false;
+function add_to_cart_langos_n(){
+    rendelesek[`langos_${butt_id}`][1] = document.getElementById(`langos_${butt_id}_db`).value;
+    rendelesek[`langos_${butt_id}`][2] = document.getElementById(`langos_${butt_id}_so`).checked;
+    rendelesek[`langos_${butt_id}`][3] = document.getElementById(`langos_${butt_id}_fok`).checked;
+    if (rendelesek[`langos_${butt_id}`][1] == 0){
+        rendelesek[`langos_${butt_id}`][2] = false;
+        rendelesek[`langos_${butt_id}`][3] = false;
     }
 }
-
-function add_to_cart_sajtos(){
-    rendelesek.sajtos[1] = document.getElementById("sajtos_db").value;
-    rendelesek.sajtos[2] = document.getElementById("sajtos_so").checked;
-    rendelesek.sajtos[3] = document.getElementById("sajtos_fok").checked;
-    if (rendelesek.sajtos[1] == 0){
-        rendelesek.sajtos[2] = false;
-        rendelesek.sajtos[3] = false;
-    }
-}
-
-function add_to_cart_tejfolos(){
-    rendelesek.tejfolos[1] = document.getElementById("tejfolos_db").value;
-    rendelesek.tejfolos[2] = document.getElementById("tejfolos_so").checked;
-    rendelesek.tejfolos[3] = document.getElementById("tejfolos_fok").checked;
-    if (rendelesek.tejfolos[1] == 0){
-        rendelesek.tejfolos[2] = false;
-        rendelesek.tejfolos[3] = false;
-    }
-}
-
-function add_to_cart_sajtostejfolos(){
-    rendelesek.sajtostejfolos[1] = document.getElementById("sajtostejfolos_db").value;
-    rendelesek.sajtostejfolos[2] = document.getElementById("sajtostejfolos_so").checked;
-    rendelesek.sajtostejfolos[3] = document.getElementById("sajtostejfolos_fok").checked;
-    if (rendelesek.sajtostejfolos[1] == 0){
-        rendelesek.sajtostejfolos[2] = false;
-        rendelesek.sajtostejfolos[3] = false;
-    }
-}
-
-function add_to_cart_bitang(){
-    rendelesek.bitang[1] = document.getElementById("bitang_db").value;
-    rendelesek.bitang[2] = document.getElementById("bitang_so").checked;
-    rendelesek.bitang[3] = document.getElementById("bitang_fok").checked;
-    if (rendelesek.bitang[1] == 0){
-        rendelesek.bitang[2] = false;
-        rendelesek.bitang[3] = false;
-    }
-}
-
-function add_to_cart_nutellas(){
-    rendelesek.nutellas[1] = document.getElementById("nutellas_db").value;
-    rendelesek.nutellas[2] = document.getElementById("nutellas_so").checked;
-    rendelesek.nutellas[3] = document.getElementById("nutellas_fok").checked;
-    if (rendelesek.nutellas[1] == 0){
-        rendelesek.nutellas[2] = false;
-        rendelesek.nutellas[3] = false;
-    }
-}
-
-
 
 let menu = document.querySelector('.head .navbar');
 document.querySelector('#bars').onclick = () => {
@@ -85,3 +40,24 @@ window.onscroll = () => {
     menu.classList.remove('active');
     searchfrm.classList.remove('active');
 }
+
+document.querySelectorAll('button').forEach(occurence => {
+    let id = occurence.getAttribute('id');
+    occurence.addEventListener('click', function() {
+    console.log('A button with ID ' + id + ' was clicked!');
+    butt_id = id;
+    add_to_cart_langos_n();
+    } );
+});
+
+/*
+function add_to_cart_sima(){
+    rendelesek.sima[1] = document.getElementById("sima_db").value;
+    rendelesek.sima[2] = document.getElementById("sima_so").checked;
+    rendelesek.sima[3] = document.getElementById("sima_fok").checked;
+    if (rendelesek.sima[1] == 0){
+        rendelesek.sima[2] = false;
+        rendelesek.sima[3] = false;
+    }
+}
+*/
